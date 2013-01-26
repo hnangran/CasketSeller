af0a128cb08f5692a0ff19cffaeeac368bc9a049
Geoffrey <geoffdobson@gmail.com>
2013-01-26 17:01:14 -0500

    added spinners ext material and color

--- src/com/example/casketseller/activities/SearchScreen.java
+++ src/com/example/casketseller/activities/SearchScreen.java
@@ -12,13 +12,76 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class SearchScreen extends Activity {
 
+	String[] casketColors;
+	String[] extMaterials;
+	String selectedColor;
+	String selectedExtMaterial;
+	
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_search_screen);
+    
+    //Set up the Exterior Material selection spinner
+    
+    extMaterials = getResources().getStringArray(R.array.extMaterials);
+    
+    Spinner extMaterialSpinner = (Spinner)findViewById(R.id.extMaterialSpinner);
+    
+    ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, 
+    		android.R.layout.simple_spinner_item, extMaterials);
+   
+    extMaterialSpinner.setAdapter(adapter1);
+    extMaterialSpinner.setOnItemSelectedListener(new OnItemSelectedListener()
+    {
+    	@Override
+    	public void onItemSelected(AdapterView<?> arg0, View arg1,
+    			int arg2, long arg3)
+    	{
+    		int index = arg0.getSelectedItemPosition();
+    		Toast.makeText(getBaseContext(), "You have selected item : " + extMaterials[index], 
+    				Toast.LENGTH_SHORT).show();
+    		selectedExtMaterial = extMaterials[index];
+    	}
+    	
+    	@Override
+    	public void onNothingSelected(AdapterView<?> arg0) {}
+    });     
+    
+    //Set up the Color selection spinner
+    
+    casketColors = getResources().getStringArray(R.array.casketColors);
+    
+    Spinner colorSpinner = (Spinner)findViewById(R.id.colorSpinner);
+    
+    ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, 
+    		android.R.layout.simple_spinner_item, casketColors);
+   
+    colorSpinner.setAdapter(adapter2);
+    colorSpinner.setOnItemSelectedListener(new OnItemSelectedListener()
+    {
+    	@Override
+    	public void onItemSelected(AdapterView<?> arg0, View arg1,
+    			int arg2, long arg3)
+    	{
+    		int index = arg0.getSelectedItemPosition();
+    		Toast.makeText(getBaseContext(), "You have selected item : " + casketColors[index], 
+    				Toast.LENGTH_SHORT).show();
+    		selectedColor = casketColors[index];
+    	}
+    	
+    	@Override
+    	public void onNothingSelected(AdapterView<?> arg0) {}
+    });     
+    
   }
 
   public void onSearch(View v) {
